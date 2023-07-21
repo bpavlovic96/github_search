3e5e05bbe4bb30f7db3d275dd4efef969b1aae51
bpavlovic96 <business.broots@gmail.com>
2023-07-21 22:28:49 +0200

    Initial commit

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,13 @@
+function SearchBox() {
+  return (
+    <>
+      <div>
+        <p>GitHub username:</p>
+        <input type="text" placeholder="Enter username..." />
+        <button>GO!</button>
+      </div>
+    </>
+  );
+}
+
+export default SearchBox;
